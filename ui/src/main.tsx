8e803783f33498cce067eb6395d6c5fcabd4f624
huseyininnc <huseyin.innc3304@gmail.com>
2025-11-13 01:56:53 +0300

    for workflow

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@mysten/dapp-kit/dist/index.css";
 import "@radix-ui/themes/styles.css";
-
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Theme } from "@radix-ui/themes";
